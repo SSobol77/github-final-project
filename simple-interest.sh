b66cec6a08642e8b55be369c4cbe4730601e584a
Siergej Sobolewski <108773983+SSobol77@users.noreply.github.com>
2023-05-08 01:11:23 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors: 
+   # SSobol77
 
    # Input:
    # p, principal amount
